2e4404e58f8763ae2a2f08c921a9d7a1cfec3124
imai.tetsu <imai.tetsu@canon.co.jp>
2019-05-29 13:57:38 +0900

    temporary add records

---
+++ records/precache-manifest.55e41737be3f839498b83ab62225ca6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9c0212326324eb59e5b6b1363bc9d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7ec666a219158efb251",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ef430fc5c5896c55526b",
+    "url": "/static/js/2.31d20c7b.chunk.js"
+  },
+  {
+    "revision": "c7ec666a219158efb251",
+    "url": "/static/js/main.42eb5302.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
