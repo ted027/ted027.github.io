f74a6b0696cfc468b4e4e99993a5ce74edf64baa
CircleCI <circleci@example.com>
2019-07-15 16:06:24 +0000

    update records webui Mon Jul 15 16:06:24 UTC 2019

--- records/precache-manifest.3aac5302e33c9db284e8980649249398.js
+++ records/precache-manifest.3aac5302e33c9db284e8980649249398.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e4901efa455fe217037e14f8a07d32b7",
+    "revision": "c9ce87c73effce8cb5f9ae8f715bf037",
     "url": "/index.html"
   },
   {
-    "revision": "63a451e583612f723ef0",
+    "revision": "c1008bc537b458f6249f",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.f44854e6.chunk.js"
   },
   {
-    "revision": "63a451e583612f723ef0",
-    "url": "/static/js/main.b31cd29a.chunk.js"
+    "revision": "c1008bc537b458f6249f",
+    "url": "/static/js/main.74944808.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
