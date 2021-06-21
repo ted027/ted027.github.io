9bf77e6e6d11952eb41610128ad5e0dbee02be66
CircleCI <circleci@example.com>
2021-06-21 10:23:00 +0000

    update records webui Mon Jun 21 10:23:00 UTC 2021

---
+++ records/precache-manifest.541042063331195c3da6011601377ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faf3f1da00a18d49f722db9a93172c9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c8e2d901a8c077dc684",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "a63d8a36fa4d728d609e",
+    "url": "/static/js/2.3799e38b.chunk.js"
+  },
+  {
+    "revision": "4cc491aecfb42b8152f3e6418c6abfe3",
+    "url": "/static/js/2.3799e38b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c8e2d901a8c077dc684",
+    "url": "/static/js/main.b4926e7b.chunk.js"
+  },
+  {
+    "revision": "7f505cb8b9ddbc703a55",
+    "url": "/static/js/runtime-main.1403132f.js"
+  }
+]);
