d37122bb98626f060dec11042271419d7a0407e2
CircleCI <circleci@example.com>
2021-09-26 14:24:02 +0000

    update records webui Sun Sep 26 14:24:02 UTC 2021

--- records/service-worker.js
+++ records/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.842f3d64becf0fff87a412f640842b92.js"
+  "/precache-manifest.155d42b7d45ecfea812232d783afe178.js"
 );
 
 self.addEventListener('message', (event) => {
