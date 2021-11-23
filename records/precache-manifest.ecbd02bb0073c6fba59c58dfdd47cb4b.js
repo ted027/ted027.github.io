d5e21b1225d7f943333de1901d55c1346082a865
CircleCI <circleci@example.com>
2021-11-23 13:34:45 +0000

    update records webui Tue Nov 23 13:34:45 UTC 2021

---
+++ records/precache-manifest.ecbd02bb0073c6fba59c58dfdd47cb4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2b805232abecd0e5bebd3c548ac402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ea3f2db4ce5ea6b3e36",
+    "url": "/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "a63d8a36fa4d728d609e",
+    "url": "/static/js/2.3799e38b.chunk.js"
+  },
+  {
+    "revision": "4cc491aecfb42b8152f3e6418c6abfe3",
+    "url": "/static/js/2.3799e38b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ea3f2db4ce5ea6b3e36",
+    "url": "/static/js/main.9f5aca6b.chunk.js"
+  },
+  {
+    "revision": "7f505cb8b9ddbc703a55",
+    "url": "/static/js/runtime-main.1403132f.js"
+  }
+]);
--- records/service-worker.js
+++ records/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea9131d5fa4f1558fb5b011e5850a878.js"
+  "/precache-manifest.ecbd02bb0073c6fba59c58dfdd47cb4b.js"
 );
 
 self.addEventListener('message', (event) => {
